b58a397a7297d098f32afd35ef40c7a7a6937088
aravindsureshvechoor <aravindsuresh601@gmail.com>
2024-01-10 12:00:21 +0530

    tokenapisgenerated

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,13 @@
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status,permissions
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-from .serializers import(UserSignupSerializer,UserSerializer)
+from .serializers import(UserSignupSerializer,UserSerializer,MyTokenObtainPairSerializer)
 from rest_framework.views import exception_handler
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import InvalidToken
 import jwt
 
 # Create your views here
@@ -26,3 +28,14 @@ class Signup(APIView):
         user            = serializer.data
 
         return Response(user, status=status.HTTP_201_CREATED)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)  # Call the parent class's post method
+        except InvalidToken as e:
+            response = Response({'detail': str(e)}, status=e.status_code)
+        return response
