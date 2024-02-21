790af9d6f1a35e29c59e8a821e52d611073b55af
aravindsureshvechoor <aravindsuresh601@gmail.com>
2024-02-21 23:32:30 +0530

    asgi file configured

--- batman/asgi.py
+++ batman/asgi.py
@@ -1,28 +1,50 @@
-"""
-ASGI config for batman project.
+# """
+# ASGI config for batman project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+# It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+# """
+
+# import os
+
+# from .channelsmiddleware import JwtAuthMiddleware
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from Chatapp.routing import websocket_urlpatterns
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batman.settings')
+# from posts.routing import websocket_urlpatterns as notification_url
+
+# application = ProtocolTypeRouter(
+#     {
+#         'http': get_asgi_application(),
+#         'websocket': JwtAuthMiddleware(
+#             AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns+notification_url))
+#         )
+#     }
+# )
 
 import os
 
-from .channelsmiddleware import JwtAuthMiddleware
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from Chatapp.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batman.settings')
 from posts.routing import websocket_urlpatterns as notification_url
 
-application = ProtocolTypeRouter(
-    {
-        'http': get_asgi_application(),
-        'websocket': JwtAuthMiddleware(
-            AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns+notification_url))
-        )
-    }
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batman.settings')
+
+def application(scope):
+    # Importing JwtAuthMiddleware here to defer the import until the application is called
+    from .channelsmiddleware import JwtAuthMiddleware
+    return ProtocolTypeRouter(
+        {
+            'http': get_asgi_application(),
+            'websocket': JwtAuthMiddleware(
+                AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns + notification_url))
+            )
+        }
+    )
