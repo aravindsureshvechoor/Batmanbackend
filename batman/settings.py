123842405d32e41fbaa8d2c2392c7642a8a980ff
aravindsureshvechoor <aravindsuresh601@gmail.com>
2024-02-21 21:19:46 +0530

    altered the settings.py file

--- batman/settings.py
+++ batman/settings.py
@@ -153,8 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
@@ -224,41 +224,3 @@ REST_FRAMEWORK = {
 }
 
 
-
-# from datetime import timedelta
-# SIMPLE_JWT = {
-#   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-#   'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
-#   'ROTATE_REFRESH_TOKENS': False,
-#   'BLACKLIST_AFTER_ROTATION': True,
-#   'UPDATE_LAST_LOGIN': False,
-
-#   'ALGORITHM': 'HS256',
-#   'SIGNING_KEY': SECRET_KEY,
-#   'VERIFYING_KEY': None,
-#   'AUDIENCE': None,
-#   'ISSUER': None,
-
-#   'AUTH_HEADER_TYPES': ('Bearer',),
-#   'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-#   'USER_ID_FIELD': 'id',
-#   'USER_ID_CLAIM': 'user_id',
-#   'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-
-#   'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-#   'TOKEN_TYPE_CLAIM': 'token_type',
-
-#   'JTI_CLAIM': 'jti',
-
-#   'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-#   'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-#   'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-
-#   # custom
-#   'AUTH_COOKIE': 'access_token',  # Cookie name. Enables cookies if value is set.
-#   'AUTH_COOKIE_DOMAIN': None,     # A string like "example.com", or None for standard domain cookie.
-#   'AUTH_COOKIE_SECURE': False,    # Whether the auth cookies should be secure (https:// only).
-#   'AUTH_COOKIE_HTTP_ONLY' : True, # Http only cookie flag.It's not fetch by javascript.
-#   'AUTH_COOKIE_PATH': '/',        # The path of the auth cookie.
-#   'AUTH_COOKIE_SAMESITE': 'Lax',  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
-# }
