cbf4a44e5cfbea68bb494e2bae4dbed1fffba084
aravindsureshvechoor <aravindsuresh601@gmail.com>
2024-02-22 11:13:30 +0530

    two lines added at the beginning of settings.py

--- batman/settings.py
+++ batman/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import django
+django.setup()
 from pathlib import Path
 from datetime import timedelta
 import os
