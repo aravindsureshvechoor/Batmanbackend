48ee6363ec02ab3ca7108c2c01d34cc30804b0d2
aravindsureshvechoor <aravindsuresh601@gmail.com>
2024-02-22 17:04:33 +0530

    imported django and setup in asgi

--- batman/asgi.py
+++ batman/asgi.py
@@ -8,14 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batman.settings')
+django.setup()
 from .channelsmiddleware import JwtAuthMiddleware
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from Chatapp.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batman.settings')
 from posts.routing import websocket_urlpatterns as notification_url
 
 application = ProtocolTypeRouter(
