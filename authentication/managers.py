5fc5aca0c0b86528a889194cb2c0bd31f42514c7
aravindsureshvechoor <aravindsuresh601@gmail.com>
2024-02-23 15:23:17 +0530

    back to normal

--- authentication/managers.py
+++ authentication/managers.py
@@ -2,28 +2,28 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email,password=None):
+    def create_user(self, email, first_name,last_name,gender,password=None):
         if not email:
             raise ValueError("Users must have an email address")
         
         email = self.normalize_email(email)
         email = email.lower()
         user = self.model(
-            # first_name=first_name,
-            # last_name=last_name,
+            first_name=first_name,
+            last_name=last_name,
             email=email,
-            # gender=gender,
+            gender=gender,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email,password=None):
+    def create_superuser(self, email, first_name, last_name,password=None):
         user = self.create_user(
             email=email,
-            # first_name=first_name,
-            # last_name=last_name,
+            first_name=first_name,
+            last_name=last_name,
             password=password,
         )
         user.is_staff = True
